a34b21eeb4e3b26b35a5477f3347d0ba129d81f9
Alpha S <alphaoumar210@yahoo.fr>
2023-04-17 16:24:15 +0200

    moment et lodash

---
+++ jour1/03-fonction.js
@@ -0,0 +1,17 @@
+// composant 
+
+///import {}
+///import {}
+///import {}
+
+/* function Composant(){
+    // ......
+    return 
+}
+export default Composant ; */
+
+function dessinerTriangle(){
+    console.log("je suis un triangle")
+}
+
+module.exports = dessinerTriangle ;  // équivalent de export default 
---
+++ jour1/03-main.js
@@ -0,0 +1,27 @@
+// import { addition, soustraction } from "./03-lib.js"
+// ES d'import 
+
+// CJS 
+// conseil => bonne pratique 
+// require au debut
+// chargé MAIS non utilisé
+/* const { addition, soustraction } = require("./03-lib") ; 
+const { a, getId , phrase , test} = require("./03-exemple")
+const dessinerTriangle = require("./03-fonction")
+const tutu = require("./03-fonction") ;  */// possibilité de renommer le module.exports = 
+const {division , puissance , multiplication} = require("./03-short")
+
+division(10 , 2) // 5
+puissance(3 , 2) // 9
+multiplication(3 , 2) // 6
+
+/*addition(1,2); // 3
+soustraction( 10 , 5 ); // 5
+console.log(a);
+console.log(phrase);
+getId(); 
+test()
+dessinerTriangle(); 
+tutu();  */
+
+// node 03-main.js
