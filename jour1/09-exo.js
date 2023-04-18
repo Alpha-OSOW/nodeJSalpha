675c1f1635ea9d2efc726982ace9494be4600ebc
Alpha S <alphaoumar210@yahoo.fr>
2023-04-18 12:22:01 +0200

    2eme jour post get

--- jour1/09-exo.js
+++ jour1/09-exo.js
@@ -1,1 +1,25 @@
-const _=require("lodash")
+const _ = require("lodash");
+
+// solution lodash et sa méthode random
+const r = [];
+for(let i = 0 ; i < 10 ; i++){
+    r.push( _.random(0,100) )
+}
+
+console.log(r);
+
+// solution sans librairie
+const r2 = [];
+for(let i = 0 ; i < 10 ; i++){
+    r2.push( Math.round(Math.random() * 100 ) )
+}
+console.log(r2);
+
+// ok
+console.log(
+    _.fill(Array(10),0)
+    .map(el=>_.random(0,100))
+)
+
+// autre solution possible 
+console.log( Array.from( new Array(10) , function(){ return _.random(0,100) } ))
